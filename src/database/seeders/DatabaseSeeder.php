2f145212115061c457ea689ec586f4a759e7d9db
HikaruShiho <shiho97797@gmail.com>
2024-04-21 17:58:16 +0900

    疎通確認

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * モデルがイベントをディスパッチしないようにする
+     * https://laravel.com/docs/11.x/seeding#muting-model-events
+     */
+    use WithoutModelEvents;
+    
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Demo\DemoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/demo', [DemoController::class, 'index']);
